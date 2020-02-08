76160c2116d786670c41741588c5a526df72fd41
zacanger <zac@zacanger.com>
2020-02-08 16:38:20 -0700

    feat: add all the things

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+export const one = () => 'one'
+export const two = () => 'two'
+export const three = () => 'three'
+export const four = () => 'four'
+export const numbers = () => [one, two, three, four].map((a) => a()).join(' ')
+
 export default () => {
   // eslint-disable-next-line no-console
-  console.log('hello there')
+  console.log(numbers())
 }
