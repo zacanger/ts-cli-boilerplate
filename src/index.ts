d4016d5941488a8226eb7fd75bdac7f933de1379
zacanger <zac@zacanger.com>
2020-02-08 17:39:58 -0700

    feat: interactivity example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import prompt from 'zeelib/lib/term-prompt'
+
 export const one = () => 'one'
 export const two = () => 'two'
 export const three = () => 'three'
@@ -5,6 +7,10 @@ export const four = () => 'four'
 export const numbers = () => [one, two, three, four].map((a) => a()).join(' ')
 
 export default () => {
-  // eslint-disable-next-line no-console
-  console.log(numbers())
+  prompt('Should I log some numbers?', true).then((answer: boolean) => {
+    if (answer) {
+      // eslint-disable-next-line no-console
+      console.log(numbers())
+    }
+  })
 }
